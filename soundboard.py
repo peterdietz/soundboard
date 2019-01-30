10b56927a3808eab6b5a8b96cb4db7c3959d8aab
Peter Dietz <pdietz@onshift.com>
2019-01-30 12:38:49 -0500

    Use pygame music instead of sounds

--- soundboard.py
+++ soundboard.py
@@ -12,12 +12,13 @@ class Soundboard:
     soundboard.playSound(0)
     """
     def __init__(self):
+        pygame.init()
         pygame.mixer.init(48000, -16, 1, 1024)
         self.soundboard = {
-            '0': pygame.mixer.Sound("sounds/buzzer.wav"),
-            '1': pygame.mixer.Sound("sounds/clap.wav"),
-            '2': pygame.mixer.Sound("sounds/CastleThunder.wav"),
-            '3': pygame.mixer.Sound("sounds/whats-happening.mp3")
+            '0': "sounds/buzzer.wav",
+            '1': "sounds/clap.wav",
+            '2': "sounds/CastleThunder.wav",
+            '3': "sounds/whats-happening.mp3"
         }
         print("Soundboard Ready.")
 
@@ -25,7 +26,9 @@ class Soundboard:
         try:
             print(key)
             if key in self.soundboard:
-                self.soundboard[key].play()
+                file_name = self.soundboard[key]
+                pygame.mixer.music.load(file_name)
+                pygame.mixer.music.play()
             else:
                 print("No sound registered at {}".format(key))
         except Exception as e:
