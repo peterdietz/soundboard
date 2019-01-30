c81744b48392deec12e897974b18651ee0115351
Peter Dietz <pdietz@onshift.com>
2019-01-30 11:33:13 -0500

    Wire soundboard into keypad

---
+++ keypad.py
@@ -0,0 +1,31 @@
+from pad4pi import rpi_gpio
+import time
+from soundboard import Soundboard
+
+
+soundboard = Soundboard()
+
+
+def process_key(key):
+    print(key)
+    soundboard.play_sound(key)
+
+
+# Setup Keypad
+KEYPAD = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["*", "0", "#"]
+]
+
+ROW_PINS = [19, 26, 20, 21] # BCM numbering
+COL_PINS = [5, 6, 13] # BCM numbering
+factory = rpi_gpio.KeypadFactory()
+keypad = factory.create_keypad(keypad=KEYPAD, row_pins=ROW_PINS, col_pins=COL_PINS)
+keypad.registerKeyPressHandler(process_key)
+
+
+while 1:
+    time.sleep(1)
+keypad.cleanup()
